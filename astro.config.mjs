a246d61a2f8716bd5220f968ab8ceceabf197612
liang-tian-yu <liang_tian_yu@foxmail.com>
2026-01-16 12:39:26 +0800

    feat: replace katex with mathjax

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import expressiveCode from "astro-expressive-code";
 import icon from "astro-icon";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeComponents from "rehype-components"; /* Render the custom directive content */
-import rehypeKatex from "rehype-katex";
+import rehypeMathjax from "rehype-mathjax";
 import rehypeSlug from "rehype-slug";
 import remarkDirective from "remark-directive"; /* Handle directives */
 import remarkGithubAdmonitionsToDirectives from "remark-github-admonitions-to-directives";
@@ -23,6 +23,7 @@ import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.
 import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
 import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
+import { s } from "hastscript";
 
 // https://astro.build/config
 export default defineConfig({
@@ -111,7 +112,17 @@ export default defineConfig({
 			parseDirectiveNode,
 		],
 		rehypePlugins: [
-			rehypeKatex,
+			[
+				rehypeMathjax,
+				{
+					tex: {
+						packages: ["base", "ams"]
+					},
+					svg: {
+						fontCache: "local"
+					},
+				}
+			],
 			rehypeSlug,
 			[
 				rehypeComponents,
